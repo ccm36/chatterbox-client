60c0b46299903559d14cf5327de708128e2f4954
ccm36 <cameron.c.malloy@gmail.com>
2017-10-14 10:50:33 -0500

    Post message to server & retrieve newest messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,19 +1,21 @@
 var App = function() {
   this.server = 'http://parse.atx.hackreactor.com/chatterbox/classes/messages';
-  
+  this.roomname = 'lobby';
+  this.username = 'defaultUser';
 };
 
 App.prototype.init = function() {
   var answer = this.fetch();
-  
+  // what is the goal for init?
 };
 
 App.prototype.send = function(message) {
+  console.log('sending message');
   $.ajax({
   // This is the url you should use to communicate with the parse API server.
     url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
     type: 'POST',
-    data: (message),
+    data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent');
@@ -29,7 +31,7 @@ App.prototype.fetch = function() {
   var that = this;
   $.ajax({
   // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
+    url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages?order=-createdAt',
     type: 'GET',
     // data: JSON.stringify(message),
     contentType: 'application/json',
@@ -46,11 +48,21 @@ App.prototype.fetch = function() {
   });
 };
 
+App.prototype.renderMessageObject = function(username, roomname, text) {
+  return {
+    username: username,
+    roomname: roomname,
+    text: text
+  };
+};
+
 App.prototype.clearMessages = function() {
   $('#chats').html('');
 };
 
 App.prototype.renderMessage = function(message) {
+  // add class to <p> tag that signifies which chatroom it belongs  
+
   var renderedMessage = $('<p>' + message.username + '<br></br>' + message.text + '</p>');
   $('#chats').append(renderedMessage);
 };
@@ -64,6 +76,15 @@ var app = new App();
 $(document).ready(function() {
   
   app.init();
+  $('#post').on('submit', function(e) {
+    e.preventDefault();
+    console.log('submitted');
+    // console.log(($(this))[0].elements.message.value);
+    var userMessage = ($(this))[0].elements.message.value;
+    var newObj = app.renderMessageObject(app.username, app.roomname, userMessage);
+    app.send(newObj);
+    app.fetch();
+  }); 
 });
 
 //On Sumbit click //
@@ -84,9 +105,7 @@ $.ajax({
 });
 */
 //on sumbit
-$('#submit').on('click', function() {
-  console.log('clicked');
-}); 
+
 //message = input data
 // $.ajax({
 //   // This is the url you should use to communicate with the parse API server.
